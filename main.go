20468eb7852cc85dd9eac4028ecb6f39db5fe559
taqin <cp@imtaqin.id>
2026-06-07 17:07:59 +0700

    chore(v0.6.2): bump version to 0.6.2

--- main.go
+++ main.go
@@ -145,7 +145,7 @@ func main() {
 		ui.OptsStatusBar().
 			FixedPart(ui.DpiX(220), "Ready").
 			FlexPart(1, truncateMid(baseDir, 70)).
-			FixedPart(ui.DpiX(90), "GoAMPP v0.6.1"),
+			FixedPart(ui.DpiX(90), "GoAMPP v0.6.2"),
 	)
 
 	wnd.On().WmCreate(func(p ui.WmCreate) int {
